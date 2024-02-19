f254a1da3d50b43aa1a5ef01f3e0e462a566e635
darshan45672 <drshnbhandary@gmail.com>
2024-02-19 07:44:57 +0530

    changed footer

--- dashboard/admin/footer.php
+++ dashboard/admin/footer.php
@@ -1,1 +1,1 @@
-<footer class="main" style="margin-top:20px"><strong>POWER ZONE </strong> By Adarsh, Amarnath and Aloke</footer>
+<footer class="main" style="margin-top:20px"><strong>POWER ZONE </strong> By XYZ</footer>
